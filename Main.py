9656078618b1657553f0ec284ef05071b0805434
tikisi <tik.hime.141193@gmail.com>
2024-03-03 12:34:25 +0900

    モデル作成前

--- Main.py
+++ Main.py
@@ -1,10 +1,16 @@
 import numpy as np
 import pandas as pd
+import PreProcessing as pp
+import tensorflow_decision_forests as tfdf
 
 train_file_path = 'C:\Users\ishii\github\KaglleHousePrices\input\house-prices-advanced-regression-techniques\train.csv'
 
 
 class Main:
     train = pd.read_csv(train_file_path)
-    train = train.drop('Id', axis = 1)
+    train = train.drop('Id', axis=1)
+    train_ds, valid_ds = pp.split_dataset(train, 0.2)
+
+    label = 'SalePrice'
+    train_ds = tfdf.keras.pd_dataframe_to_tf_dataset(train_ds, label=label, task=tfdf.keras.Task.REGRESSION)
 
--- PreProcessing/PreProcessing.py
+++ PreProcessing/PreProcessing.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import numpy as np
 
 
 class PreProcessing:
     def split_dataset(dataset, valid_ratio):
-        valid_
+        valid_indices = np.random.rand(len(dataset)) < valid_ratio
+        return dataset[~valid_indices], dataset[valid_indices]
