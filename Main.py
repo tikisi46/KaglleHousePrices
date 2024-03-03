b4dfa9ffa2de48f6ea5ef4ad13e2f990f7895d32
tikisi <tik.hime.141193@gmail.com>
2024-03-03 23:18:51 +0900

    trainが存在しないエラーが発生

--- Main.py
+++ Main.py
@@ -1,16 +1,29 @@
 import numpy as np
 import pandas as pd
-import PreProcessing as pp
-import tensorflow_decision_forests as tfdf
+import tensorflow as tf
 
-train_file_path = 'C:\Users\ishii\github\KaglleHousePrices\input\house-prices-advanced-regression-techniques\train.csv'
+import PreProcessing
+
+train_file_path = r"C:\Users\ishii\github\KaglleHousePrices\input\house-prices-advanced-regression-techniques\train.csv"
 
 
 class Main:
+    label = 'SalePrice'
+
     train = pd.read_csv(train_file_path)
     train = train.drop('Id', axis=1)
-    train_ds, valid_ds = pp.split_dataset(train, 0.2)
 
-    label = 'SalePrice'
-    train_ds = tfdf.keras.pd_dataframe_to_tf_dataset(train_ds, label=label, task=tfdf.keras.Task.REGRESSION)
+    quantitative = [f for f in train.columns if train.dtypes[f] != 'object']
+    quantitative.remove('SalePrice')
+    qualitative = [f for f in train.columns if train.dtypes[f] == 'object']
+
+    preProcessing = PreProcessing()
+
+    qual_encoded = []
+    for q in qualitative:
+        preProcessing.encode(train, q)
+        qual_encoded.append(q + '_E')
+
+    train_ds, valid_ds = preProcessing.split_dataset(train, 0.2)
+
 
--- PreProcessing/PreProcessing.py
+++ PreProcessing/PreProcessing.py
@@ -3,6 +3,18 @@ import numpy as np
 
 
 class PreProcessing:
-    def split_dataset(dataset, valid_ratio):
+    def split_dataset(self, dataset, valid_ratio):
         valid_indices = np.random.rand(len(dataset)) < valid_ratio
         return dataset[~valid_indices], dataset[valid_indices]
+
+    def encode(self, dataset, feature):
+        ordering = pd.DataFrame()
+        ordering['val'] = dataset[feature].unique()
+        ordering.index = ordering.values
+        ordering['spmean'] = dataset[[feature, 'SalePrice']].groupby(feature).mean()['SalePrice']
+        ordering = ordering.sort_values('spmean')
+        ordering['ordering'] = range(1, ordering.shape[0] + 1)
+        ordering = ordering['ordering'].to_dict()
+
+        for cat, o in ordering.items():
+            dataset.loc[dataset[feature] == cat, feature + '_E'] = o
